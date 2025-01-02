0837273a0359ec3a55c9b2b2df9f0c13d2f9c8e9
Lorenzo <lorenzo.weffort@aluno.ifsp.edu.br>
2025-01-02 11:09:30 -0300

    Teste inicial de credenciais

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    credentials_str = os.getenv('GOOGLE_OAUTH_CREDENTIALS')
+
+    if credentials_str:
+        credentials = json.loads(credentials_str)
+        token = credentials.get('token')
+        print(f'Token: {token}')
+        return render_template('index.html')
+    else:
+        print("As credenciais não foram encontradas.")
+        return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
