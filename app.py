c1a521819ead94bffd9698b589fe901ce41c4e15
Lorenzo <lorenzo.weffort@aluno.ifsp.edu.br>
2025-01-03 20:05:25 -0300

    Adicionado minutos customizados para execução

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
 from flask import Flask, render_template, jsonify, redirect
 import requests
-from datetime import datetime, timedelta
-import time
+from datetime import datetime, timedelta, time
+import pytz
 
 app = Flask(__name__)
 
-# Variáveis globais para acompanhar o estado
 last_access_time = None
 next_access_time = None
 time_remaining = None
 last_time_checked = None
 
-TIMEZONE = "America/Sao_Paulo"
-URL_TO_ACCESS = "https://youtube-monitor.onrender.com"
+TIMEZONE = pytz.timezone("America/Sao_Paulo")
+# URL_TO_ACCESS = "https://youtube-monitor.onrender.com"
+URL_TO_ACCESS = "https://google.com"
 
+HOUR_TO_ACCESS = 20  # 0-23
+MINUTES_TO_ACCESS = 20  # 0-59
+# SECONDS_TO_ACCESS = 10 - Fixed
 
 @app.route('/access_url')
 def access_url():
     global last_access_time, next_access_time, time_remaining, last_time_checked
 
-    now = datetime.now()
+    now = datetime.now(TIMEZONE)
     last_time_checked = now
 
-    # Realiza o acesso à URL
     try:
         requests.get(URL_TO_ACCESS)
         last_access_time = now
-        print(f"Acesso realizado em {last_access_time}")
+        print(f"Acesso realizado à {URL_TO_ACCESS} em {last_access_time}")
     except Exception as e:
         print(f"Erro ao acessar a URL: {e}")
 
-    # Calcula o próximo horário de acesso (00:00:10 do dia seguinte)
-    next_day = now.date() + timedelta(days=1)
-    next_access_time = datetime.combine(next_day, datetime.min.time()) + timedelta(seconds=10)
+    # Calcula o próximo horário de acesso considerando a hora e os minutos
+    next_day = (now + timedelta(days=1)).date()
+    next_access_time = datetime.combine(next_day, time(hour=HOUR_TO_ACCESS, minute=MINUTES_TO_ACCESS), tzinfo=TIMEZONE) + timedelta(seconds=10)
 
     # Calcula o tempo restante para o próximo acesso
-    # time_remaining = (next_access_time - now).total_seconds()
-    time_remaining = 60
+    time_remaining = (next_access_time - now).total_seconds()
 
     # Redireciona para a página principal
     return redirect('/index')
-
 
 @app.route('/index')
 def index():
@@ -51,11 +51,10 @@ def index():
                            last_time_checked=last_time_checked)
 
 @app.route('/load_test')
-
 def load_test():
     global time_remaining
-    time_remaining = 60
-    
+    time_remaining = 10
+
     return redirect('/index')
 
 @app.route('/')
