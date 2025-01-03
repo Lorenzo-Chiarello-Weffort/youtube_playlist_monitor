b4685fb65a7c4044ab0a29ac7a97847e90a4c832
Lorenzo-Chiarello-Weffort <100961865+Lorenzo-Chiarello-Weffort@users.noreply.github.com>
2025-01-03 15:33:06 -0300

    Agendador para o render

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ time_remaining = None
 last_time_checked = None
 
 TIMEZONE = "America/Sao_Paulo"
-URL_TO_ACCESS = "https://www.google.com"
+URL_TO_ACCESS = "https://youtube-monitor.onrender.com"
 
 
 @app.route('/')
@@ -35,7 +35,8 @@ def access_url():
     next_access_time = datetime.combine(next_day, datetime.min.time()) + timedelta(seconds=10)
 
     # Calcula o tempo restante para o próximo acesso
-    time_remaining = (next_access_time - now).total_seconds()
+    # time_remaining = (next_access_time - now).total_seconds()
+    time_remaining = 7200
 
     # Redireciona para a página principal
     return redirect('/index')
@@ -46,14 +47,8 @@ def index():
     return render_template("index.html",
                            last_access_time=last_access_time,
                            next_access_time=next_access_time,
-                           time_remaining=20,
+                           time_remaining=time_remaining,
                            last_time_checked=last_time_checked)
-
-@app.route('/wait')
-def wait():
-    time.sleep(30)
-    return redirect('/')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
