d03beb393b177916eba13ad19c6dad200b0625b3
Lorenzo-Chiarello-Weffort <100961865+Lorenzo-Chiarello-Weffort@users.noreply.github.com>
2025-01-03 09:53:41 -0300

    Mudado checagem

--- app.py
+++ app.py
@@ -10,16 +10,19 @@ app = Flask(__name__)
 last_access_time = None
 next_access_time = None
 time_remaining = None
+last_time_checked = None
 
 TIMEZONE = "America/Sao_Paulo"
 URL_TO_ACCESS = "http://google.com"
 
 
 def scheduler():
-    global last_access_time, next_access_time, time_remaining
+    global last_access_time, next_access_time, time_remaining, last_time_checked
 
     while True:
+        print("Executando checagem...")
         now = datetime.now(TIMEZONE)
+        last_time_checked = now
 
         # Calcula o próximo horário de acesso (00:00:10 do dia seguinte)
         if not next_access_time or now >= next_access_time:
@@ -39,7 +42,8 @@ def scheduler():
         time_remaining = (next_access_time - datetime.now(TIMEZONE)).total_seconds()
 
         # Aguarda até o próximo acesso ou atualiza os valores em intervalos curtos
-        time.sleep(min(time_remaining, 60))
+        # time.sleep(min(time_remaining, 60))
+        time.sleep(10)
 
 
 @app.route('/')
@@ -47,7 +51,8 @@ def index():
     return render_template("index.html",
                            last_access_time=last_access_time,
                            next_access_time=next_access_time,
-                           time_remaining=time_remaining)
+                           time_remaining=time_remaining,
+                           last_time_checked)
 
 
 if __name__ == "__main__":
